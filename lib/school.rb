3b3ad863317c1992523f085fe225a0d243347176
Kyle Samuel <Ksamuel92@gmail.com>
2021-03-10 10:24:12 -0500

    commit tree e275f9bae29aaffef7274f8185c8107c5fada04e

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+require "pry"
+class School
+    attr_accessor :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade].nil?
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+       end
+
+       def grade(grade)
+        @roster[grade]
+       end
+
+       def sort
+        @roster.each_with_object({}) do |(grade, students), a|
+            a[grade] = students.sort
+        end
+       end
+end
